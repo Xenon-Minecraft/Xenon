b4d3751a35d1a5555df0be83419b5dc8aba153c3
Sebastian Agius <theultimatepleb@gmail.com>
2018-09-08 17:27:12 +1000

    Add function to send Packets, and add fields relating to authentication

--- src/main/kotlin/com/github/xenonminecraft/network/PlayerConnection.kt
+++ src/main/kotlin/com/github/xenonminecraft/network/PlayerConnection.kt
@@ -1,11 +1,24 @@
 package com.github.xenonminecraft.network
 
+import com.github.xenonminecraft.network.auth.GameProfile
+import com.github.xenonminecraft.network.packet.Packet
+import io.netty.channel.socket.SocketChannel
 import java.net.InetSocketAddress
+import javax.crypto.SecretKey
 
-class PlayerConnection(val addr: InetSocketAddress) {
+class PlayerConnection(val addr: InetSocketAddress, val ch: SocketChannel) {
     var state: State = State.NONE
     var compressionThreshold: Int = -1
     var protocolVersion: Int = 0
+
+    var sharedSecret: SecretKey? = null
+    var verifyToken: ByteArray? = null
+    var username: String? = null
+    var gameProfile: GameProfile? = null
+
+    fun sendPacket(p: Packet) {
+        ch.writeAndFlush(p)
+    }
 
     enum class State(val id: Int) {
         NONE(0),
