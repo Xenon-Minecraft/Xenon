2aee526bc5e4590a1d98a59fab96a28453d8d3c0
Sebastian Agius <theultimatepleb@gmail.com>
2018-09-08 17:55:55 +1000

    Add method to enable compression

--- src/main/kotlin/com/github/xenonminecraft/network/PlayerConnection.kt
+++ src/main/kotlin/com/github/xenonminecraft/network/PlayerConnection.kt
@@ -2,8 +2,7 @@ package com.github.xenonminecraft.network
 
 import com.github.xenonminecraft.Xenon
 import com.github.xenonminecraft.network.auth.GameProfile
-import com.github.xenonminecraft.network.netty.PacketEncryptingDecoder
-import com.github.xenonminecraft.network.netty.PacketEncryptingEncoder
+import com.github.xenonminecraft.network.netty.*
 import com.github.xenonminecraft.network.packet.Packet
 import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.socket.SocketChannel
@@ -11,13 +10,15 @@ import java.net.InetSocketAddress
 import javax.crypto.SecretKey
 
 class PlayerConnection(val addr: InetSocketAddress, val ch: SocketChannel, val channelContext: ChannelHandlerContext) {
-    private var state: State = State.NONE
+    var state: State = State.NONE
+    var protocolVersion: Int = 0
+
     var compressionThreshold: Int = -1
-    private var protocolVersion: Int = 0
+    private var compress = false
 
     private var encrypted = false
-    private var sharedSecret: SecretKey? = null
-    private var verifyToken: ByteArray? = null
+    var sharedSecret: SecretKey? = null
+    var verifyToken: ByteArray? = null
     var username: String? = null
     var gameProfile: GameProfile? = null
 
@@ -29,10 +30,18 @@ class PlayerConnection(val addr: InetSocketAddress, val ch: SocketChannel, val c
         if (encrypted)
             return
         channelContext.channel().pipeline().addBefore("splitter", "decrypt",
-                PacketEncryptingDecoder(Xenon.instance!!.encryptionManager.createNetCipher(2, sharedSecret!!)))
+                PacketEncryptionDecoder(Xenon.instance!!.encryptionManager.createNetCipher(2, sharedSecret!!)))
         channelContext.channel().pipeline().addBefore("prepender", "encrypt",
-                PacketEncryptingEncoder(Xenon.instance!!.encryptionManager.createNetCipher(1, sharedSecret!!)))
+                PacketEncryptionEncoder(Xenon.instance!!.encryptionManager.createNetCipher(1, sharedSecret!!)))
         encrypted = true
+    }
+
+    fun enableCompression() {
+        if(compress)
+            return
+        channelContext.channel().pipeline().addAfter("splitter", "decompress", MinecraftPacketDecompressor(this))
+        channelContext.channel().pipeline().addAfter("prepender", "compress", MinecraftPacketCompressor(this))
+        compress = true
     }
 
     enum class State(val id: Int) {
