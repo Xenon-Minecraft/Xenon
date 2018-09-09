95e04af86c9947b5d9137668df8939e68d8ead24
Sebastian Agius <theultimatepleb@gmail.com>
2018-09-09 17:06:56 +1000

    Stop trying to edit direct buffer

--- src/main/kotlin/com/github/xenonminecraft/network/netty/PacketEncryptionTranslator.kt
+++ src/main/kotlin/com/github/xenonminecraft/network/netty/PacketEncryptionTranslator.kt
@@ -1,6 +1,7 @@
 package com.github.xenonminecraft.network.netty
 
 import io.netty.buffer.ByteBuf
+import io.netty.buffer.Unpooled
 import io.netty.channel.ChannelHandlerContext
 import javax.crypto.Cipher
 
@@ -8,7 +9,7 @@ class PacketEncryptionTranslator(val cipher: Cipher) {
 
     fun decipher(ctx: ChannelHandlerContext, buf: ByteBuf): ByteBuf {
         val readable = buf.readableBytes()
-        val array = buf.array()
+        val array = Unpooled.copiedBuffer(buf).array()
 
         val heapBuffer = ctx.alloc().heapBuffer(cipher.getOutputSize(readable))
         heapBuffer.writerIndex(cipher.update(array, 0, readable, heapBuffer.array(), heapBuffer.arrayOffset()))
@@ -17,12 +18,17 @@ class PacketEncryptionTranslator(val cipher: Cipher) {
     }
 
     fun cipher(inBuf: ByteBuf, outBuf: ByteBuf) {
-        val readable = inBuf.readableBytes()
-        val array = inBuf.array()
-        val size = cipher.getOutputSize(readable)
+        try {
+            val readable = inBuf.readableBytes()
+            val array = Unpooled.copiedBuffer(inBuf).array()
+            val size = cipher.getOutputSize(readable)
 
-        val buff = ByteArray(size)
+            val buff = ByteArray(size)
 
-        outBuf.writeBytes(buff, 0, cipher.update(array, 0, readable, buff))
+            outBuf.writeBytes(buff, 0, cipher.update(array, 0, readable, buff))
+        } catch(e: Exception) {
+            e.printStackTrace()
+        }
+
     }
 }
